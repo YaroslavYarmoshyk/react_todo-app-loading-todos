4425992122728bb93cb3106b43b1ba06fdc7ca70
Yaroslav <yarmoshyk96@gmail.com>
2025-07-13 23:52:13 +0300

    Add fixes after review

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
 import { getTodos, USER_ID } from './api/todos';
 import { Todo } from './types/Todo';
 import classNames from 'classnames';
 
+export const ErrorMessages = Object.freeze({
+  LOAD_TODOS: 'Unable to load todos',
+  EMPTY_TITLE: 'Title should not be empty',
+  ADD_TODO: 'Unable to add a todo',
+  DELETE_TODO: 'Unable to delete a todo',
+  UPDATE_TODO: 'Unable to update a todo',
+});
+
 type TodoStatus = 'all' | 'active' | 'completed';
 
+const filterTodos = (todos: Todo[], status: TodoStatus): Todo[] => {
+  if (status === 'active') {
+    return todos.filter(todo => !todo.completed);
+  }
+
+  if (status === 'completed') {
+    return todos.filter(todo => todo.completed);
+  }
+
+  return todos;
+};
+
 export const App: React.FC = () => {
-  const [todos, setTodos] = React.useState<Todo[]>([]);
-  const [visibleTodos, setVisibleTodos] = React.useState<Todo[]>([]);
-  const [status, setStatus] = React.useState<TodoStatus>('all');
-  const [loading, setLoading] = React.useState(true);
-  const [errorMessage, setErrorMessage] = React.useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
+  const [status, setStatus] = useState<TodoStatus>('all');
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleStatusChange = (newStatus: TodoStatus) => {
-    setStatus(newStatus);
-
-    let filteredTodos = [...todos];
-
-    if (newStatus !== 'all') {
-      filteredTodos = filteredTodos.filter(todo =>
-        newStatus === 'completed' ? todo.completed : !todo.completed,
-      );
+  useEffect(() => {
+    if (!USER_ID) {
+      return;
     }
 
-    setVisibleTodos(filteredTodos);
-  };
+    const load = async () => {
+      setLoading(true);
+      setErrorMessage('');
 
-  React.useEffect(() => {
+      try {
+        const loadedTodos = await getTodos();
+
+        setTodos(loadedTodos);
+        setVisibleTodos(loadedTodos);
+      } catch {
+        setErrorMessage('Unable to load todos');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    void load();
+  }, []);
+
+  useEffect(() => {
     if (errorMessage) {
       const timeoutId = setTimeout(() => {
         setErrorMessage('');
@@ -41,33 +71,25 @@ export const App: React.FC = () => {
     return () => {};
   }, [errorMessage]);
 
-  React.useEffect(() => {
-    if (!USER_ID) {
-      return;
+  useEffect(() => {
+    setVisibleTodos(filterTodos(todos, status));
+  }, [todos, status]);
+
+  const handleStatusChange = (newStatus: TodoStatus) => {
+    setStatus(newStatus);
+  };
+
+  useEffect(() => {
+    if (errorMessage) {
+      const timeoutId = setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+
+      return () => clearTimeout(timeoutId);
     }
 
-    const fetchTodos = () => {
-      setLoading(true);
-      setErrorMessage('');
-
-      const load = async () => {
-        try {
-          const loadedTodos = await getTodos();
-
-          setTodos(loadedTodos);
-          setVisibleTodos(loadedTodos);
-        } catch {
-          setErrorMessage('Unable to load todos');
-        } finally {
-          setLoading(false);
-        }
-      };
-
-      load().catch(() => {});
-    };
-
-    fetchTodos();
-  }, []);
+    return () => {};
+  }, [errorMessage]);
 
   if (!USER_ID) {
     return <UserWarning />;
@@ -187,15 +209,6 @@ export const App: React.FC = () => {
           onClick={() => setErrorMessage('')}
         />
         {errorMessage}
-        {/*Unable to load todos*/}
-        {/*<br />*/}
-        {/*Title should not be empty*/}
-        {/*<br />*/}
-        {/*Unable to add a todo*/}
-        {/*<br />*/}
-        {/*Unable to delete a todo*/}
-        {/*<br />*/}
-        {/*Unable to update a todo*/}
       </div>
     </div>
   );
